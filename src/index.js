646d3a4d22f7dfabbb952671c2abff2d73d7d0a8
C-Martha <marthacornejo2@gmail.com>
2017-05-09 22:59:44 -0400

    Add SPA for GitHub Pages boilerplate

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './Components/App';
-
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
 );
