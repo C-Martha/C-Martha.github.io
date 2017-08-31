3bd3d26a2e577db2f34d307253a9d9d5163f1c8b
C-Martha <marthacornejo2@gmail.com>
2017-08-31 01:46:54 -0400

    Fix experience description

--- src/components/App.js
+++ src/components/App.js
@@ -43,7 +43,7 @@ class App extends React.Component {
             title="Software Developer Intern"
             time="January 2017 - March 2017"
             technologies = { technologiesCH }
-            description="Joined the team of developers at Code Hangar where I helped contribute to the release of a new version of ReQLPro. ReQLPro is a RethinkDB GUI Client for Mac, Windows, and Linux. While working there I also had the chance to be part of sprint meetings and pair programming sessions. "/>
+            description="Joined the team of developers at Code Hangar where I helped contribute to the release of a new version of ReQLPro. ReQLPro is a RethinkDB GUI Client for Mac, Windows, and Linux. While working there I also had the opportunity to be part of sprint meetings and pair programming sessions. "/>
 
           <Divider inset={false} />
 
@@ -53,7 +53,7 @@ class App extends React.Component {
             title="Software Developer Intern "
             time="June 2016 - December 2016"
             technologies = { technologiesSF }
-            description="While interning at SoFriendly I had the chance to work on building websites using Webflow and Wordpress for clients. Aside from the design and technical skills that I learn during my intership at SoFriendly, another big take away was becoming a more confident developer thanks to the support and motivation from the team." />
+            description="While interning at SoFriendly I had the chance to work on building websites using Webflow and Wordpress for clients. Aside from the design and technical skills that I learned during my intership at SoFriendly, another big take away was becoming a more confident developer thanks to the support and motivation from the team." />
         </Card>
 
         <Card className="card">
